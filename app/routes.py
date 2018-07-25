511c4945dec1a98d22c5fbf5839a07ac0e1375f7
Hotko Egor <egorhotko@gmail.com>
2018-07-26 00:59:46 +0300

    Add login page

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,25 @@
 from . import app
+from flask import render_template
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'Egor'}
+    posts = [
+        {'author': {'username': 'Jonh'},
+         'body': 'Beatifull day in Belarus!'
+         },
+        {'author': {'username': 'Susan'},
+         'body': 'The Avengers movie was so cool!'
+         }
 
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sing In', form=form)
